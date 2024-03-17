aaa466e07218a4611f865342c22d3133b424573a
Vinos Sarhad <vinos.sarhadd@gmail.com>
2024-03-18 01:49:52 +0300

    done

---
+++ src/components/DisplayData.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+function DisplayData({firstName,lastName}){
+    return(
+
+        <div>
+            <p>fdsfc</p>
+      <h1>{firstName}sds</h1>
+      <h1>{lastName}</h1>
+    </div>
+    )
+}
+
+export default DisplayData
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Form from './components/Form'
-
+import ParentForm from './components/ParentForm';
 ReactDOM.render(
-  <Form />,
+  <ParentForm />,
   document.getElementById('root')
 );
